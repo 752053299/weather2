d15c1db27c5b2407df826a3825eebc02222013bd
752053299 <752053299@qq.com>
2017-12-11 20:03:50 +0800

    继续编写城市查询页

--- app/src/main/java/com/example/guiwu/myweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/guiwu/myweather/ChooseAreaFragment.java
@@ -4,22 +4,37 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.guiwu.myweather.db.City;
 import com.example.guiwu.myweather.db.Country;
 import com.example.guiwu.myweather.db.Province;
+import com.example.guiwu.myweather.util.HttpUtil;
+import com.example.guiwu.myweather.util.Utility;
 
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
- * Created by 乔先森 on 2017/12/10.
+ * Created by 乔先森 on 2017/12/10. 选择地区的fragment
  */
 
 public class ChooseAreaFragment extends Fragment {
@@ -31,6 +46,7 @@ public class ChooseAreaFragment extends Fragment {
     private Button mBcakButton;
     private RecyclerView mRecyclerView;
     private List<String> datalist = new ArrayList<>();
+    private AreaAdapter recyclerViewAdapter;
 
     /**
      * 省列表
@@ -74,7 +90,65 @@ public class ChooseAreaFragment extends Fragment {
         titleText = (TextView) view.findViewById(R.id.title_text);
         mBcakButton = (Button) view.findViewById(R.id.back_button);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), OrientationHelper.VERTICAL,false));
+
+        updateUI();
+
         return view;
+    }
+
+    /**
+     * rercyclerView的初始化
+     */
+    private void updateUI(){
+        queryProvinces();
+
+        recyclerViewAdapter = new AreaAdapter(datalist);
+
+        mRecyclerView.setAdapter(recyclerViewAdapter);
+        recyclerViewAdapter.notifyDataSetChanged();
+    }
+
+    private class AreaHolder extends RecyclerView.ViewHolder {
+
+        private TextView mAreaName;
+
+
+        public AreaHolder(View itemView) {
+            super(itemView);
+
+            mAreaName = (TextView) itemView.findViewById(R.id.area_name);
+        }
+
+        public void bind(String name){
+            mAreaName.setText(name);
+        }
+    }
+
+    private class AreaAdapter extends RecyclerView.Adapter<AreaHolder>{
+
+        private List<String> areaNames;
+
+        public AreaAdapter(List<String> datalist){
+            areaNames = datalist;
+        }
+
+        @Override
+        public AreaHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View itemView = inflater.inflate(R.layout.city_list,parent,false);
+            return new AreaHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(AreaHolder holder, int position) {
+            holder.bind(areaNames.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return areaNames.size();
+        }
     }
 
 
@@ -85,5 +159,93 @@ public class ChooseAreaFragment extends Fragment {
     private void queryProvinces(){
         titleText.setText("中国");
         mBcakButton.setVisibility(View.GONE);
+        mProvinceList = DataSupport.findAll(Province.class);
+        if (mProvinceList.size() > 0){  //数据库有数据
+            datalist.clear();
+            for (Province province : mProvinceList){
+                datalist.add(province.getProvienceName());
+            }
+            currentLeavel = LEVEL_PROVINCE;
+        }else { //数据库没数据
+            String address = "http://guolin.tech/api/china/";
+            queryFromServer(address,"province");
+        }
+    }
+
+    /**
+     * 查询全国所有的市，规则同上
+     */
+
+    private void queryCities(){
+
+    }
+
+    /**
+     * 从服务器查询数据
+     * @param address url地址
+     * @param type 查询的级别
+     */
+    private void queryFromServer(String address,final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequset(address, new Callback()  {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)){
+                    result = Utility.handleProvinceResponse(responseText);
+                }else if ("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText,slectedProvince.getProvienceNum());
+                }else if ("country".equals(type)){
+                    result = Utility.handleCityResponse(responseText,slectedCountry.getCityId());
+                }
+                if (result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)){
+                                queryProvinces();
+                            }else if ("city".equals(type)){
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+    /**
+     * 显示对话框方法
+     */
+    private void showProgressDialog(){
+        if (mProgressDialog != null){
+            mProgressDialog = new ProgressDialog(getActivity());
+            mProgressDialog.setMessage("正在加载");
+            mProgressDialog.setCanceledOnTouchOutside(false);
+        }
+        mProgressDialog.show();
+    }
+
+    /**
+     * 关闭对话框方法
+     */
+    private void closeProgressDialog(){
+        if (mProgressDialog != null){
+            mProgressDialog.dismiss();
+        }
     }
 }
--- app/src/main/java/com/example/guiwu/myweather/MainActivity.java
+++ app/src/main/java/com/example/guiwu/myweather/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.guiwu.myweather;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
+        if (fragment == null){
+            fragment = new ChooseAreaFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
     }
 }
